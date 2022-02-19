2b3114ab5dbc18aadd5bc0b9bdcdaad2624fed20
Rachel7748 <90146882+Rachel7748@users.noreply.github.com>
2022-02-19 17:19:56 +0200

    Create login_token.py

---
+++ login_token.py
@@ -0,0 +1,29 @@
+
+
+class Token():
+
+        def __init__(self, _id, name, role):
+            self._id = id
+            self._name = name
+            self._role = role
+
+        @property
+        def id(self):
+            return self._id
+
+        @property
+        def name(self):
+            return self._name
+
+        @property
+        def role(self):
+            return self._role
+
+        def __repr__(self):
+            return  f'{self._id}, {self._name},{self._role}'
+
+        def __str__(self):
+            return f'id:{self._id}, name: {self._name}, role:{self._role}'
+
+
+
